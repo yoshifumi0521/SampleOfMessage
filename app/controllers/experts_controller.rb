eadff51f3aac694fd2a6ab5a2e746ffa7cdb64a1
YoshifumiSato <yoshifumi0521@gmail.com>
2012-12-10 16:31:44 +0900

    ログインユーザー以外のエキスパートをすべて表示。

--- app/controllers/experts_controller.rb
+++ app/controllers/experts_controller.rb
@@ -3,8 +3,19 @@
 class ExpertsController < ApplicationController
   
   def index
-    logger.debug("index-------")  
-  
+    
+    #ログアウトした場合は、このindexアクションをreturnする。
+    if !@current_user
+      return
+    end
+    #expertモデルで、ユーザーのidを取得する。
+    @current_userId =  @current_user.id.to_s
+
+    #SQL文で、expertの人と、ログインユーザー以外を取り出す。
+    @experts = User.find_by_sql(["select * from users 
+    WHERE advice = ? and not id = ?",true, @current_userId ]) 
+
+    logger.debug(@experts)
   
   end
 
