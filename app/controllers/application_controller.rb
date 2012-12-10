803ae0e6252347dfc9670149a70f1fc38882d513
YoshifumiSato <yoshifumi0521@gmail.com>
2012-12-10 14:36:21 +0900

    ログイン機能の実装完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
+#coding: utf-8
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :authorize
+
+  private
+  #すべてのコントローラーのアクションの実行時にこの処理を行う。
+  def authorize
+    
+    if cookies.signed[:user_id]
+      #ログインユーザーのオブジェクトを@current_user変数にいれる。
+      @current_user = User.find_by_id(cookies.signed[:user_id])
+      #クッキー情報があったとしても、ユーザー情報がなくてnilだったら、クッキーを削除する。 
+      cookies.delete :user_id unless @current_user
+    end
+ 
+ 
+ end
+
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,15 +2,33 @@
 #ログインするときのコントローラー
 class SessionsController < ApplicationController
 
-  def create
-    logger.debug(params)
+  #applicationコントローラーのauthorizeメソッドは実行しないようにする。
+  skip_before_filter :authorize
 
-  
+  def create
+    
+    @user = User.find_by_name_and_password(params[:name],params[:password])
+    logger.debug(@user.inspect)
+    
+    if !@user
+      redirect_to :root
+    else
+      #ここでクッキーをいれる。
+      cookies.signed[:user_id] ={ value: @user.id ,expires: 30.days.from_now } 
+      redirect_to :root
+    end
+
+
   end
 
-  
+  #クッキーを削除する。  
   def destroy
-
+  
+    if cookies.signed[:user_id] == params[:id].to_i
+      #クッキーを削除する。
+      cookies.delete :user_id
+      redirect_to :root
+    end
 
   end
 
