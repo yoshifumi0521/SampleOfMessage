79ece9187e28a6ef323aa3003040a9b92ea31ab0
YoshifumiSato <yoshifumi0521@gmail.com>
2012-12-10 10:01:08 +0900

    Railsアプリを追加した

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleOfMessage::Application.config.secret_token = '9f870e319d9f8866791be8a80db9e9d59f0efe463ad9fa8a36738c792602e9d5b6c3f0ea1b0fcc49c01a7e6750baf1f970a3466e3b5d13048aa4f4c455149c11'
